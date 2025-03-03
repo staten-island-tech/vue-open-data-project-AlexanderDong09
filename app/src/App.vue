1603a91f6dc40d21d83c6d76e9e9704dbc46ed25
AlexanderDong09 <alexanderdong09@gmail.com>
2025-03-03 12:32:17 -0500

    yreas

---
+++ app/src/App.vue
@@ -0,0 +1,88 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="we;cpom" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  top: 0;
+  left: auto;
+  right: a;
+  width: 100%;
+  z-index: 1000;
+  border-bottom: 2px solid var(--color-border);
+}
+
+.wrapper {
+  display: flex;
+  flex-direction: column; /* Always vertical */
+  align-items: center;
+  text-align: center;
+}
+
+nav {
+  width: 100%;
+  font-size: 1rem;
+  text-align: center;
+  margin-top: 0.5rem;
+}
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
