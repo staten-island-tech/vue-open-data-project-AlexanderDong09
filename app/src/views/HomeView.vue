468e01272779f0b42ec8a733054fa0cf8181a516
AlexanderDong09 <alexanderdong09@gmail.com>
2025-03-04 12:32:00 -0500

    w

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,5 +1,21 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+const childrenwithlead = ref('')
+async function getData() {
+  let result = await fetch('https://data.cityofnewyork.us/resource/tnry-kwh5.json')
+  let data = await result.json()
+  childrenwithlead.value = data
+}
+
+console.log(childrenwithlead)
+
+onMounted(() => {
+  getData()
+})
+</script>
 
 <template>
-  <main></main>
+  <main>
+    <div></div>
+  </main>
 </template>
