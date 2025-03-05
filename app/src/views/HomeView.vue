e618f872011630e09752186fff62bde4d3e7e190
AlexanderDong09 <alexanderdong09@gmail.com>
2025-03-05 12:33:51 -0500

    theres lead in my blood

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -16,6 +16,11 @@ onMounted(() => {
 
 <template>
   <main>
-    <div></div>
+    <div v-for="item in childrenwithlead">
+      <h2>
+        {{ item.geo_area_name }} in the year {{ item.time_period }} had
+        {{ item.children_under_6_years_with }} kids with LEAD in their BLOOD
+      </h2>
+    </div>
   </main>
 </template>
