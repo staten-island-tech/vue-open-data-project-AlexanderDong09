d4bf6781f743857e546ced21af966a3714b8e64b
AlexanderDong09 <alexanderdong09@gmail.com>
2025-03-24 11:51:32 -0400

    pie xhart

--- app/src/components/DoughnutChart.vue
+++ app/src/components/DoughnutChart.vue
@@ -3,18 +3,21 @@
 </template>
 
 <script setup>
+import { computed } from 'vue'
 import { Doughnut } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, CategoryScale, ArcElement } from 'chart.js'
 
 ChartJS.register(Title, Tooltip, Legend, CategoryScale, ArcElement)
+
 const props = defineProps({
-  data: Array,
+  childrenwithlead: Array,
 })
-const chartData = {
-  labels: ['Brooklyn', 'Manhattan', 'Staten Island', 'Bronx', 'Queens'],
+
+const chartData = computed(() => ({
+  labels: props.childrenwithlead.map((item) => `${item.geo_area_name} - ${item.time_period}`),
   datasets: [
     {
-      data: props.data,
+      data: props.childrenwithlead.map((item) => item.children_under_6_years_with),
       backgroundColor: [
         'rgb(255, 99, 20)',
         'rgb(54, 40, 100)',
@@ -22,10 +25,28 @@ const chartData = {
         'rgb(46, 205, 86)',
         'rgb(76, 205, 255)',
       ],
+      datalabels: {
+        formatter: (value) => `${value} kids with lead`,
+        color: '#fff',
+        font: {
+          weight: 'bold',
+        },
+      },
     },
   ],
-}
+}))
+
 const chartOptions = {
   responsive: true,
+  plugins: {
+    tooltip: {
+      callbacks: {
+        label: (tooltipItem) => {
+          let value = tooltipItem.raw
+          return `${value} kids with lead`
+        },
+      },
+    },
+  },
 }
 </script>
--- app/src/views/AboutView.vue
+++ app/src/views/AboutView.vue
@@ -1,9 +1,37 @@
 <template>
   <div class="">
-    <DoughnutChart :data="[43, 45, 23, 54, 29]" />
+    <DoughnutChart :childrenwithlead="childrenwithlead" />
   </div>
 </template>
 
 <script setup>
 import DoughnutChart from '@/components/DoughnutChart.vue'
+import { ref, onMounted } from 'vue'
+
+const childrenwithlead = ref([])
+
+async function getData() {
+  try {
+    const response = await fetch('https://data.cityofnewyork.us/resource/tnry-kwh5.json')
+    if (response.status != 200) {
+      throw new Error(response)
+    } else {
+      let data = await response.json()
+      childrenwithlead.value = data
+      // console.log(childrenwithlead)
+
+      const areaNames = childrenwithlead.value.map((el) => el.geo_area_name)
+      const areaNamesButUnique = new Set(areaNames)
+      uniqueAreaNames.value = Array.from(areaNamesButUnique)
+      // RAHGGGGGGGGGGGGGGGGhHHHHHHHHHHHHHHH !!!!!!!!!!!!!!!!!!!!!!!!!
+    }
+  } catch (error) {
+    console.log(error)
+    console.log('sorry coudlnt fid that')
+  }
+}
+
+onMounted(() => {
+  getData()
+})
 </script>
